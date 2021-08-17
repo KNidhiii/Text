697279df35ab90cf1a3904f779acc2489c3f3d82
kumarin <kumarin@CIN-PF2Z5QHV>
2021-08-17 16:26:12 +0530

    Add project files.

---
+++ Text/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Kumarin\Documents\text.txt";
+            Console.WriteLine("Enter the input string: ");
+
+            var input = Console.ReadLine();
+
+            string line;
+            int counter = 0;
+            int count = 0;
+            int lineNum = 0;
+            StreamReader file = new StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                var a = line.Split(" ");
+                foreach (var x in a)
+                {
+                    if (x.Equals(input))
+                    {
+                        count++;
+                        if (count == 2)
+                        {
+                            lineNum = counter + 1;
+                        }
+                    }
+                }
+                counter++;
+            }
+            Console.WriteLine("Total occurance of {0} is {1}", input, count);
+            Console.WriteLine("2nd occurance of {0} is in line {1}", input, lineNum);
+
+            file.Close();
+
+
+
+        }
+    }
+}
